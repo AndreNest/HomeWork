b95bf14cf2fb5c973e5af9641e8d0b2748b6e28e
AndreyNesterov <1@nestandre.ru>
2022-06-29 00:05:30 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите  трёхначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100)
+{
+    Console.WriteLine("В числе нет третьей цифры");
+}
+else
+{
+    int thirdNumber = (number % 100) % 10;
+Console.WriteLine(thirdNumber);
+}
