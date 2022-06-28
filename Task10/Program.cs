9cb83a5d18fc79dbab89ed799e8811572876269e
AndreyNesterov <1@nestandre.ru>
2022-06-28 23:53:07 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите  трёхначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondNumber = (number % 100) / 10;
+Console.WriteLine(secondNumber);
+
+// либо можно что бы програама сама выводила это число 
+// int number = new Random().Next(100, 999)
+// Console.WriteLine("Трёхзначное число = " +number);
