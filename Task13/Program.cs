77cdc46b2521445212e206769be5604aa2df256c
AndreyNesterov <1@nestandre.ru>
2022-06-30 16:13:12 +0300

    version 3.2

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,19 +1,22 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 Console.WriteLine("Введите  трёхначное число");
-int number = Convert.ToInt32(Console.ReadLine());
+string number = 1337.ToString(Console.ReadLine());
 Console.WriteLine("------------------------------");
-if(number > 99 && number < 1000)
-{
-    int thirdNumber = (number % 100) % 10;
-    Console.WriteLine("Третья цифра = " +thirdNumber);
+Console.WriteLine(number[2]);
+
+
+// if(number > 99 && number < 1000)
+// {
+//     int thirdNumber = (number % 100) % 10;
+//     Console.WriteLine("Третья цифра = " +thirdNumber);
     
-}
-if(number > 999 && number < 10000)
-{
-    int thirdNumber2 = (number % 1000) % 100 / 10;
-    Console.WriteLine("Третья цифра = " +thirdNumber2);
-}
-else
-{
-    Console.WriteLine("В числе нет третьей цифры");;
-}
+// }
+// if(number > 999 && number < 10000)
+// {
+//     int thirdNumber2 = (number % 1000) % 100 / 10;
+//     Console.WriteLine("Третья цифра = " +thirdNumber2);
+// }
+// else
+// {
+//     Console.WriteLine("В числе нет третьей цифры");;
+// }
